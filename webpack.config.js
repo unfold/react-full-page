b588d469cf77964b40df56b94413572fb3b8ce37
Daniel Mahal <danielmahal@gmail.com>
2014-11-21 11:09:57 +0100

    Indentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
 var client = {
-    context: __dirname + '/src',
-    entry: './client',
+  context: __dirname + '/src',
+  entry: './client',
 
-    output: {
-        path: __dirname + '/build',
-        publicPath: 'build',
-        filename: 'client.js'
-    },
+  output: {
+    path: __dirname + '/build',
+    publicPath: 'build',
+    filename: 'client.js'
+  },
 
-    module: {
-      loaders: [
-        { test: /.js$/, loader: 'jsx?harmony&insertPragma=React.DOM' }
-      ]
-    }
+  module: {
+    loaders: [
+      { test: /.js$/, loader: 'jsx?harmony&insertPragma=React.DOM' }
+    ]
+  }
 }
 
 var server = {
-    target: 'node',
-    context: __dirname + '/src',
-    entry: './server',
+  target: 'node',
+  context: __dirname + '/src',
+  entry: './server',
 
-    output: {
-        path: __dirname + '/build',
-        libraryTarget: 'commonjs2',
-        filename: 'server.js'
-    },
+  output: {
+    path: __dirname + '/build',
+    libraryTarget: 'commonjs2',
+    filename: 'server.js'
+  },
 
-    module: {
-      loaders: [
-        { test: /.js$/, loader: 'jsx?harmony&insertPragma=React.DOM' }
-      ]
-    }
+  module: {
+    loaders: [
+      { test: /.js$/, loader: 'jsx?harmony&insertPragma=React.DOM' }
+    ]
+  }
 }
 
 
